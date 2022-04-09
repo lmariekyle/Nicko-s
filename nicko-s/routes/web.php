881af24e78baf845648b4a1713efd73b7dcc62f6
lourdes <lourdesmkc@gmail.com>
2022-04-09 20:14:11 +0800

    Admin UI Panel Added

--- nicko-s/routes/web.php
+++ nicko-s/routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Admin Dashboard
+Route::get('admin', function () {
+    return view('dashboard');
+});
