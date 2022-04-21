7bf1fa5ce86918c58ee2a7d63c230add8f1ef100
miguel <mareyes2132@gmail.com>
2022-04-21 18:47:21 +0800

    Package & User Seeders

--- nicko-s/database/migrations/2022_04_21_110107_create_caterings_table.php
+++ nicko-s/database/migrations/2022_04_21_110107_create_caterings_table.php
@@ -15,11 +15,11 @@ class CreateCateringsTable extends Migration
     {
         Schema::create('caterings', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
-            $table->foreignId('catering_detail_id')->references('id')->on('catering_details');
+            $table->foreignId('package_id')->references('id')->on('packages');
             $table->foreignId('customer_id');
             $table->integer('total_payment');
             $table->enum('catering_status', ['pending', 'approved', 'rejected', 'success', 'failed']);
+            $table->timestamps();
         });
     }
 
